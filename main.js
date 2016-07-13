aa0f1a5a88b1b1d86e5ec425981f3944224f7b66
Raphael Deem <raphael.deem@gmail.com>
2016-07-12 22:08:45 -0700

    markup

--- main.js
+++ main.js
@@ -27,9 +27,6 @@ function createWindow () {
   })
 }
 
-var markdown = require( "markdown" ).markdown;
-console.log( markdown.toHTML( "Hello *World*!" ) );
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
